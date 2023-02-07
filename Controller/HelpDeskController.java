d8cc256c94920f610112a6b0a87f638e6d980c41
Samuel John <samjohnmalayam@gmail.com>
2023-02-07 19:31:53 +0530

    Basic framework updated.

---
+++ Controller/HelpDeskController.java
@@ -0,0 +1,15 @@
+package HelpDesk.Controller;
+
+import HelpDesk.*;
+
+public class HelpDeskController {   
+    public HelpDeskController() {
+
+    } 
+    public Request getRequestById(String id) {
+        return null; //TODO
+    }
+    public boolean postRequest(Request req) {
+        return false;
+    }
+}
---
+++ Request.java
@@ -0,0 +1,18 @@
+package HelpDesk;
+
+public class Request {
+   String randId, userName, userRollNum, userBranch, userSem, requestTitle, requestDescription, userEmail;
+   int userBatch;
+   
+   public Request(/*All instance variables except Id*/) {
+    /*
+     * Main body
+     * 
+     * 
+     */
+    generateId();
+    }
+
+   private void generateId() {
+   }
+}
